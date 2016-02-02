bb5d3a7c6a1af1274ca64059c256edd214de5c29
Antti Möttönen <antti.moettoenen@gmail.com>
2016-02-02 14:45:36 +0200

    Initial commit with few layout files and autogenerated setttings activity

---
+++ app/src/main/java/com/moetto/amicafoodwidget/MenuProvider.java
@@ -0,0 +1,9 @@
+package com.moetto.amicafoodwidget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by moetto on 02/02/16.
+ */
+public class MenuProvider extends AppWidgetProvider {
+}
